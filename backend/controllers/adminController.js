298d0245b47cddef02ed609aed4175602fb12afd
Shreyas Gupta <technology.shreyas@gmail.com>
2021-03-10 10:01:48 +0530

    admin auth

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -3,9 +3,51 @@ const {
     key,
     sign
 } = require('../utils/jwt');
+const Admin = require('../models/Admin');
 
 const adminAuth = (req, res) => {
-
+    let email = req.body.email;
+    let password = req.body.password;
+    if (!(email && password)) {
+        res.status(400).json({
+            error: 'Required fields are missing.'
+        });
+    } else {
+        Admin.where({
+            email: email
+        }).findOne(function (err, admin) {
+            if (err) {
+                console.log(err);
+                return res.sendStatus(500);
+            }
+            if (admin) {
+                bcrypt.compare(password, admin.password).then((result) => {
+                    if (result) {
+                        sign({
+                            email: admin.email,
+                            id: admin.id,
+                            type: "admin"
+                        }, key, (err, token) => {
+                            if (err) {
+                                console.log(err);
+                                return res.sendStatus(500);
+                            }
+                            let authDisplay = {};
+                            authDisplay.authKey = token;
+                            authDisplay.data = admin;
+                            res.json(authDisplay);
+                        });
+                    } else {
+                        res.sendStatus(403);
+                    }
+                });
+            } else {
+                res.status(400).json({
+                    error: 'No admin exists with the following details.'
+                });
+            }
+        });
+    }
 }
 
 module.exports = {
