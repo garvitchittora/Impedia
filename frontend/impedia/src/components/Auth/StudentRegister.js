8081ac3d8a7f4aa7898929b8fbda9860d8a123f4
Harshdeep Singh Pruthi <2802harsh@gmail.com>
2021-03-14 21:41:42 +0530

    st register new ui + axios call

--- frontend/impedia/src/components/Auth/StudentRegister.js
+++ frontend/impedia/src/components/Auth/StudentRegister.js
@@ -1,52 +1,122 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import {
     makeStyles,
-    Grid,
+    Typography,
     FormControl,
     InputLabel,
-    Input,
     Button,
-    Typography,
+    InputAdornment,
+    IconButton,
+    FilledInput,
 } from '@material-ui/core';
+import {
+    Visibility,
+    VisibilityOff
+ } from '@material-ui/icons';
+import back1 from '../../assets/Login/login-1.svg';
+import back2 from '../../assets/Login/login-2.svg';
+import back3 from '../../assets/Login/login-3(2).svg';
+import ImpediaLogo from '../../assets/Logo-Impedia.png';
+import LoginSidePic from '../../assets/Login/loginSidePic.svg';
 
 const useStyles = makeStyles(theme => ({
-    formName: {
-        color: "red",
-        fontSize: "25px",
-        textAlign: "center",
-        padding: "30px 0",
+    loginPage: {
+        position: "absolute",
+        height:"100%",
+        width:"100%",
+        overflowX: "hidden",
     },
-    logoStyle: {
+    bg1: {
+        position:"absolute",
+        height:"25vh",
+        left:"-2%",
+        top:"-2%",
+        zIndex:"-1",
+        [theme.breakpoints.down("xs")]: {
+            top:"-12%",
+            left:"-15%",
+          }
+    },
+    bg2:{
+        position:"absolute",
+        right:"0",
+        top:"-5%",
+        height:"35vh",
+        zIndex:"-1",
+        [theme.breakpoints.down("xs")]: {
+            top:"-12%",
+            right:"-10%",
+          }
+    },
+    bg3:{
+        position:"absolute",
+        left:"0",
+        right:"0",
+        marginLeft:"auto",
+        marginRight:"auto",
+        bottom:"-30%",
+        height:"15vh",
+        zIndex:"-1"
+    },
+    logo:{
+        width:"300px"
+    },
+    loginContainer:{
+        width:"95vw",
+        margin:"5% auto",
+        display:"flex",
+        alignItems:"center",
+        justifyContent:"center",
+        flexDirection:"column",
+        verticalAlign:"middle",
+        position:"absolute",
+        left:"2.5vw",
+        [theme.breakpoints.down("xs")]: {
+            top:"10vh",
+          }
+    },
+    formName: {
+        fontSize: "25px",
+        textAlign:"center",
+        padding:"5px 0",
+        fontWeight: "600"
+    },
+    logoStyle:{
         color: "red",
         fontSize: "40px",
-        textAlign: "center",
-        fontWeight: "bold",
+        textAlign:"center",
+        fontWeight:"bold",
     },
-    GridContainerWrapper: {
-        paddingTop: "30px"
+    GridContainerWrapper:{
+        paddingTop:"30px"
     },
     formContainer: {
         display: "flex",
         flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
-        width: "90vw",
+        width: "95vw",
         maxWidth: "30rem",
         margin: "auto"
     },
     formWrapper: {
         border: "1px solid #B0B0B0",
+        maxWidth:"1200px",
         display: "flex",
-        padding: "5%",
+        padding: "2% 3%",
         alignItems: "center",
         borderRadius: "15px",
-        maxWidth: "70%",
-        margin: "auto",
+        margin: "0",
         marginBottom:"30px",
+    },
+    formCover:{
+        flex:"50%"
     },
     formInputs: {
         padding: "10px",
-        width: "100%"
+        width: "100%",
+        maxWidth:"80vw"
     },
     fieldInput: {
         width: "100%"
@@ -80,14 +150,17 @@ const useStyles = makeStyles(theme => ({
     profileInputField: {
         display: "none"
     },
-    textCenter: {
-        textAlign: "center",
+    textCenter:{
+        textAlign:"center",
     },
-    blacklogo: {
-        color: "black"
+    loginImageContainer:{
+        flex:"35%"
     },
-    yellowLogo: {
-        color: "red"
+    loginImage:{
+        width: "100%",
+        [theme.breakpoints.down("sm")]: {
+            display:"none",
+          }
     }
 
 }));
@@ -101,6 +174,14 @@ const StudentRegister = () => {
     const [BranchValues, setBranchValues] = React.useState('');
     const [PasswordValues, setPasswordValues] = React.useState('');
     const [NameValues, setNameValues] = React.useState('');
+
+    const [showPassword, setShowPassword] = useState(false);
+    const clickShowPassword = () => {
+        setShowPassword((prev)=>(!prev));
+    }
+    const mouseDownPassword = (e) => {
+        e.preventDefault();
+    }
 
     const handleChange = (event) => {
         if (event.target.name === "email") {
@@ -135,59 +216,53 @@ const StudentRegister = () => {
             name:NameValues,
         }
 
-        const requestOptions = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(body)
-        };
-
-        const response = await fetch(`/student/register`, requestOptions);
-        const data = await response.json();
-        console.log(data);
+        axios.post('/student/register',body)
+        .then((res)=>{
+            console.log(res);
+        })
+        .catch((err)=>{
+            console.log(err);
+        });
     }
 
     return (
-        <>
-            <Grid container justify="center" className={classes.GridContainerWrapper}>
-                <Grid item xs={12} md={6} className={classes.textCenter}>
-                    <img src="/image/login.svg" alt="login"/>
-                </Grid>
-                <Grid item xs={12} md={6}>
+    <div className={classes.loginPage}>
+            <img src={back1} className={classes.bg1} alt="login-bg-1"/>
+            <img src={back2} className={classes.bg2} alt="login-bg-2"/>
+            <img src={back3} className={classes.bg3} alt="login-bg-3"/>
+
+            {/* The Form */}
+                <div className={classes.loginContainer}>
                     <div>
                         <div className={classes.textCenter}>
-                            <Typography className={classes.logoStyle}>
-                                <span className={classes.blacklogo}>Impe</span><span className={classes.yellowLogo}>dia</span>
-                            </Typography>
+                            <img src={ImpediaLogo} className={classes.logo} alt="impedia-logo" />
                         </div>
-                        <Grid item>
-                            <Typography className={classes.formName}>
+            
+                            <Typography className={classes.formName} color="error">
                                 Student Registration
                             </Typography>
-                        </Grid>
                     </div>
                     <div className={classes.formWrapper}>
-                        <form className={classes.formContainer} onSubmit={submitFunction}>
-
+                        <div className={classes.formCover}>
+                            <form className={classes.formContainer} onSubmit={submitFunction}>
+                                <div className={classes.formInputs}>
+                                    <FormControl className={classes.fieldInput} variant="filled">
+                                        <InputLabel htmlFor="email">
+                                            Email
+                                        </InputLabel>
+                                        <FilledInput
+                                            id="email"
+                                            name="email"
+                                            onChange={handleChange}
+                                        />
+                                    </FormControl>
+                                </div>
                             <div className={classes.formInputs}>
-                                <FormControl className={classes.fieldInput}>
-                                    <InputLabel htmlFor="email">
-                                        Email
-        </InputLabel>
-                                    <Input
-                                        id="email"
-                                        name="email"
-                                        onChange={handleChange}
-                                    />
-                                </FormControl>
-                            </div>
-                            <div className={classes.formInputs}>
-                                <FormControl className={classes.fieldInput}>
+                                <FormControl className={classes.fieldInput} variant="filled">
                                     <InputLabel htmlFor="sem">
                                         Sem
-        </InputLabel>
-                                    <Input
+                                    </InputLabel>
+                                    <FilledInput
                                         id="sem"
                                         name="sem"
                                         onChange={handleChange}
@@ -195,11 +270,11 @@ const StudentRegister = () => {
                                 </FormControl>
                             </div>
                             <div className={classes.formInputs}>
-                                <FormControl className={classes.fieldInput}>
+                                <FormControl className={classes.fieldInput} variant="filled">
                                     <InputLabel htmlFor="section">
                                         Section
-        </InputLabel>
-                                    <Input
+                                    </InputLabel>
+                                    <FilledInput
                                         id="section"
                                         name="section"
                                         onChange={handleChange}
@@ -207,11 +282,11 @@ const StudentRegister = () => {
                                 </FormControl>
                             </div>
                             <div className={classes.formInputs}>
-                                <FormControl className={classes.fieldInput}>
+                                <FormControl className={classes.fieldInput} variant="filled">
                                     <InputLabel htmlFor="branch">
                                         Branch
-        </InputLabel>
-                                    <Input
+                                    </InputLabel>
+                                    <FilledInput
                                         id="branch"
                                         name="branch"
                                         onChange={handleChange}
@@ -219,11 +294,11 @@ const StudentRegister = () => {
                                 </FormControl>
                             </div>
                             <div className={classes.formInputs}>
-                                <FormControl className={classes.fieldInput}>
+                                <FormControl className={classes.fieldInput} variant="filled">
                                     <InputLabel htmlFor="name">
                                         Name
-        </InputLabel>
-                                    <Input
+                                    </InputLabel>
+                                    <FilledInput
                                         id="name"
                                         name="name"
                                         onChange={handleChange}
@@ -231,28 +306,42 @@ const StudentRegister = () => {
                                 </FormControl>
                             </div>
                             <div className={classes.formInputs}>
-                                <FormControl className={classes.fieldInput}>
-                                    <InputLabel htmlFor="password">
-                                        Password
-        </InputLabel>
-                                    <Input
-                                        id="password"
-                                        name="password"
-                                        onChange={handleChange}
-                                    />
+                                <FormControl className={classes.fieldInput} variant="filled">
+                                       <InputLabel htmlFor="password">Password</InputLabel>
+                                       <FilledInput
+                                           type={showPassword?'text':'password'}
+                                           id="password"
+                                           name="password"
+                                           value={PasswordValues}
+                                           onChange={handleChange}
+                                           endAdornment={
+                                               <InputAdornment position="end">
+                                               <IconButton
+                                                   aria-label="toggle password visibility"
+                                                   onClick={clickShowPassword}
+                                                   onMouseDown={mouseDownPassword}
+                                               >
+                                                   {showPassword ? <Visibility /> : <VisibilityOff />}
+                                               </IconButton>
+                                               </InputAdornment>
+                                           }
+                                        />
                                 </FormControl>
                             </div>
-
-                            <div >
-                                <Button className={classes.formButton} variant="contained" color="secondary" type="submit">
-                                    Register
-    </Button>
-                            </div>
-                        </form>
+                                <div >
+                                    <Button className={classes.formButton} variant="contained" color="secondary" type="submit">
+                                        Register
+                                    </Button>
+                                </div>
+                            </form>
+                        </div>
+                        <div className={classes.loginImageContainer}>
+                            <img src = {LoginSidePic} alt="login-side" className={classes.loginImage}/>
+                        </div>
                     </div>
-                </Grid>
-            </Grid>
-        </>
+                </div>
+
+        </div>
     )
 }
 
