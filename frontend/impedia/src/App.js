de087f7976ad40281c035b9297774b7b41978f09
garvitchittora <garvitchittora2016@gmail.com>
2021-03-17 00:30:43 +0530

    routes changed and add authority

--- frontend/impedia/src/App.js
+++ frontend/impedia/src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-
-import LoginPage from './components/Auth/LoginPage';
 import AdminLogin from './components/Auth/AdminLogin';
 import AuthorityLogin from './components/Auth/AuthorityLogin';
 import StudentLogin from './components/Auth/StudentLogin';
 import StudentRegister from './components/Auth/StudentRegister';
 import ChangeDomain from './components/Admin/ChangeDomain';
-
+import AddAuthority from './components/Admin/AddAuthority';
 
 function App() {
   return (
@@ -18,8 +16,9 @@ function App() {
           <Route exact path="/login/admin" component={AdminLogin} />
           <Route exact path="/login/authority" component={AuthorityLogin} />
           <Route exact path="/login/student" component={StudentLogin} />
-          <Route exact path="/StudentRegister" component={StudentRegister} />
-          <Route exact path="/ChangeDomain" component={ChangeDomain} />
+          <Route exact path="/register/student" component={StudentRegister} />
+          <Route exact path="/admin/ChangeDomain" component={ChangeDomain} />
+          <Route exact path="/admin/AddAuthority" component={AddAuthority} />
         </Switch>
       </BrowserRouter>
     </>
