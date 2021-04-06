9c811d2275c015ce4f5d191118a4979e6fd99cad
Harshdeep Singh Pruthi <2802harsh@gmail.com>
2021-04-06 22:23:42 +0530

    change build path app.js

--- backend/app.js
+++ backend/app.js
@@ -49,7 +49,7 @@ db.on("error", function (err) {
 if(process.env.NODE_ENV === 'production')
 {
   app.get("*", (req, res) => { 
-    res.sendFile(path.join(__dirname , "../client/build/index.html"));
+    res.sendFile(path.join(__dirname , "../frontend/impedia/build/index.html"));
     });
 }
 
