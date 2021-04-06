3e2d1d875709dc30858e3f72094312a37f4118c1
garvitchittora <garvitchittora2016@gmail.com>
2021-04-06 15:58:58 +0530

    पूरा होम पेज परीक्षण किया

--- frontend/impedia/src/components/Home/__test__/Home.test.js
+++ frontend/impedia/src/components/Home/__test__/Home.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup , fireEvent } from "@testing-library/react";
 import Home from "../Home";
 import { BrowserRouter as Router } from "react-router-dom";
 
@@ -13,4 +13,89 @@ it("should take a snapshot", () => {
   );
 
   expect(asFragment(<Home />)).toMatchSnapshot();
+});
+
+it("admin login button redirect", () => {
+    const { getByTestId } = render(
+    <Router>
+        <Home />
+    </Router>
+    ); 
+    
+    fireEvent.click(getByTestId('admin-login-button'))
+
+    expect(location.pathname).toBe('/login/admin');
+});
+
+it("authority login button redirect", () => {
+    const { getByTestId } = render(
+    <Router>
+        <Home />
+    </Router>
+    ); 
+    
+    fireEvent.click(getByTestId('authority-login-button'))
+
+    expect(location.pathname).toBe('/login/authority');
+});
+
+it("student login button redirect", () => {
+    const { getByTestId } = render(
+    <Router>
+        <Home />
+    </Router>
+    ); 
+    
+    fireEvent.click(getByTestId('student-login-button'))
+
+    expect(location.pathname).toBe('/login/student');
+});
+
+
+it("student register button redirect", () => {
+    const { getByTestId } = render(
+    <Router>
+        <Home />
+    </Router>
+    ); 
+    
+    fireEvent.click(getByTestId('student-register-button'))
+
+    expect(location.pathname).toBe('/register/student');
+});
+
+it("student login button redirect 2", () => {
+    const { getByTestId } = render(
+    <Router>
+        <Home />
+    </Router>
+    ); 
+    
+    fireEvent.click(getByTestId('student-login-button-2'))
+
+    expect(location.pathname).toBe('/login/student');
+});
+
+it("authority login button redirect", () => {
+    const { getByTestId } = render(
+    <Router>
+        <Home />
+    </Router>
+    ); 
+    
+    fireEvent.click(getByTestId('authority-login-button-2'))
+
+    expect(location.pathname).toBe('/login/authority');
+});
+
+it("admin login button redirect", () => {
+    const { getByTestId } = render(
+    <Router>
+        <Home />
+    </Router>
+    ); 
+    
+    fireEvent.click(getByTestId('admin-login-button-2'))
+
+    expect(location.pathname).toBe('/login/admin');
 });
