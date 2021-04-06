0ae319f42ee11bdf1c7b16b2967e255744515e68
Harshdeep Singh Pruthi <2802harsh@gmail.com>
2021-04-06 23:03:43 +0530

    deploy bug fixes

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ app.use(cors());
 
 if(process.env.NODE_ENV === 'production')
 {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "../frontend/impedia/build")));
 }
 
 // Adding router middleware
