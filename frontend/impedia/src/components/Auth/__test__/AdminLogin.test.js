fbf4cfdba20e9c0837b891b36d510604a3669249
Harshdeep Singh Pruthi <2802harsh@gmail.com>
2021-04-24 20:54:08 +0530

    add auth bug fix + adminlogin test changes

--- frontend/impedia/src/components/Auth/__test__/AdminLogin.test.js
+++ frontend/impedia/src/components/Auth/__test__/AdminLogin.test.js
@@ -1,11 +1,65 @@
-import React from "react";
-import { render, cleanup, fireEvent } from "@testing-library/react";
-import AdminLogin from "../AdminLogin";
+// import React from "react";
+// import { render, cleanup, fireEvent } from "@testing-library/react";
+// import AdminLogin from "../AdminLogin";
+// import { BrowserRouter as Router } from "react-router-dom";
+// import { mount, configure } from 'enzyme'
+// import puppeteer from 'puppeteer'
+// import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+// configure({ adapter: new Adapter() });
+
+// afterEach(cleanup);
+
+// it("should take a snapshot", () => {
+//   const { asFragment } = render(
+//     <Router>
+//       <AdminLogin />
+//     </Router>
+//   );
+
+//   expect(asFragment(<AdminLogin />)).toMatchSnapshot();
+// });
+
+// it("admin login", async () => {
+//   const browser = await puppeteer.launch({});
+//   const page = await browser.newPage();
+//   await page.goto('http://localhost:3000/login/admin');
+
+//   await page.waitForSelector("#email", {
+//     visible: true,
+//   });
+//   await page.click("#email", { clickCount: 1 });
+//   await page.type(
+//     "#email",
+//     "admin@admin.com",
+//   );
+//   await page.click("#password", { clickCount: 1 });
+//   await page.type(
+//     "#password",
+//     "password",
+//   );
+//   await page.click("#submit-login", { clickCount: 1 });
+//   await page.waitForSelector("a[href='/admin/addauthority']", {
+//     visible: true,
+//   });
+
+//   let pageURL = await page.evaluate(() => {
+//     console.log(location.pathname)
+// 		if(location.pathname === "/admin/dashboard"){
+//       return true;
+//     }else{
+//       return false;
+//     }
+// 	});
+
+//   expect(pageURL).toBe(true);
+
+// }, 30000);
+
+import React from 'react';
+import { render, cleanup , fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { BrowserRouter as Router } from "react-router-dom";
-import { mount, configure } from 'enzyme'
-import puppeteer from 'puppeteer'
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-configure({ adapter: new Adapter() });
+import AdminLogin from '../AdminLogin';
 
 afterEach(cleanup);
 
@@ -19,38 +73,24 @@ it("should take a snapshot", () => {
   expect(asFragment(<AdminLogin />)).toMatchSnapshot();
 });
 
-it("admin login", async () => {
-  const browser = await puppeteer.launch({});
-  const page = await browser.newPage();
-  await page.goto('http://localhost:3000/login/admin');
-
-  await page.waitForSelector("#email", {
-    visible: true,
-  });
-  await page.click("#email", { clickCount: 1 });
-  await page.type(
-    "#email",
-    "admin@admin.com",
+it("login attempt", ()=>{
+  const wrapper =  render(
+    <Router>
+      <AdminLogin />
+    </Router>
   );
-  await page.click("#password", { clickCount: 1 });
-  await page.type(
-    "#password",
-    "password",
-  );
-  await page.click("#submit-login", { clickCount: 1 });
-  await page.waitForSelector("a[href='/admin/addauthority']", {
-    visible: true,
-  });
 
-  let pageURL = await page.evaluate(() => {
-    console.log(location.pathname)
-		if(location.pathname === "/admin/dashboard"){
-      return true;
-    }else{
-      return false;
-    }
-	});
+  const email = wrapper.getByTestId("email");
+  const password = wrapper.getByTestId("password");
+  const button = wrapper.getByTestId("submit-login");
 
-  expect(pageURL).toBe(true);
+  userEvent.type(email, "admin@admin.com");
+  userEvent.type(password, "password");
+  userEvent.click(button);
 
-}, 30000);
+  setTimeout(()=>{
+
+  },4000);
+
+  expect(location.pathname).toBe('/admin/dashboard');
+})
