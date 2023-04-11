2169c1d2c8cbff7cdc3264670a0eec974c489c71
Shreyas Gupta <shreyas.gupta1@sprinklr.com>
2023-04-11 13:04:12 +0530

    strings?

---
+++ backend/strings.js
@@ -0,0 +1,7 @@
+exports.mongoURL =
+  "mongodb+srv://admin:admin@cluster0.vv3tn.mongodb.net/impedia-multiorg?retryWrites=true&w=majority";
+exports.key = "behadsecurekey";
+exports.emailCredentials = {
+    user: "imepdia.platform@gmail.com",
+    pass: "jfwyidbpqquhnwbh"
+}
